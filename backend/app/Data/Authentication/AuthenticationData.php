900a05915f6961c30a1212d348800f7bd94a7190
harmless_fun <haminhdunghl@gmail.com>
2025-12-19 18:28:13 +0700

    Sửa AuthenticationData.

--- backend/app/Data/Authentication/AuthenticationData.php
+++ backend/app/Data/Authentication/AuthenticationData.php
@@ -9,18 +9,15 @@ class AuthenticationData extends Data
     public readonly ?string $email;
     public readonly ?string $password;
     public readonly ?string $token;
-    public readonly ?string $password_confirmation;
 
     public function __construct(
         ?string $email = null,
         ?string $password = null,
         ?string $token = null,
-        ?string $password_confirmation = null,
     ) {
         $this->email = $email;
         $this->password = $password;
         $this->token = $token;
-        $this->password_confirmation = $password_confirmation;
     }
 
     public static function forLogin(string $email, string $password): self
@@ -28,23 +25,17 @@ class AuthenticationData extends Data
         return new self(email: $email, password: $password);
     }
 
-    public static function forLogout(): self
-    {
-        return new self();
-    }
-
     public static function forForgotPassword(string $email): self
     {
         return new self(email: $email);
     }
 
-    public static function forPasswordReset(string $email, string $token, string $password, string $password_confirmation): self
+    public static function forPasswordReset(string $email, string $token, string $password): self
     {
         return new self(
             email: $email,
             token: $token,
             password: $password,
-            password_confirmation: $password_confirmation
         );
     }
 }
