5d4587c1a2b6baadac1207a2d6c62bcb8f63e2db
harmless_fun <haminhdunghl@gmail.com>
2026-02-02 16:39:18 +0700

    Tạo base button component cho frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+  import BaseButton from '@/components/base/BaseButton.vue'
+</script>
+
+<template>
+  <h1>You did it!</h1>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+
+  <div style="padding: 40px">
+    <BaseButton>Log in</BaseButton>
+
+    <BaseButton block>
+      Submit Form
+    </BaseButton>
+
+    <BaseButton disabled>
+      Disabled Button
+    </BaseButton>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ frontend/src/components/base/BaseButton.vue
@@ -0,0 +1,65 @@
+<template>
+  <button
+    class="base-btn"
+    :class="[
+      `base-btn--${size}`,
+      { 'base-btn--block': block }
+    ]"
+    :disabled="disabled"
+  >
+    <slot />
+  </button>
+</template>
+
+<script setup>
+defineProps({
+  block: Boolean,
+  disabled: Boolean,
+  size: {
+    type: String,
+    default: 'md' // sm | md | lg
+  },
+})
+</script>
+
+<style scoped>
+.base-btn {
+  font-family: inherit;
+  padding: 0 24px;
+  border-radius: 32px;
+  border: 1px solid #FF383C;
+  background-color: #FF383C;
+  color: white;
+  font-weight: 500;
+  cursor: pointer;
+  transition: all 0.2s ease;
+}
+
+/* SMALL */
+.base-btn--sm {
+  font-size: 16px;
+  height: 56px;
+}
+
+/* MEDIUM (default) */
+.base-btn--md {
+  font-size: 22px;
+  height: 64px;
+}
+
+/* Full width option */
+.base-btn--block {
+  width: 100%;
+}
+
+/* Hover effect (nice UX) */
+.base-btn:hover:not(:disabled) {
+  opacity: 0.9;
+}
+
+/* Disabled */
+.base-btn:disabled {
+  opacity: 0.5;
+  cursor: not-allowed;
+}
+</style>
